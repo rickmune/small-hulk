b52d48ad74b911eacb9a6735e94311686811bc93
pmunene <rickmune@gmail.com>
2013-05-16 19:49:33 +0300

    ibase interface

--- Android/src/com/safapp/repositories/IBaseEntityRepository.java
+++ Android/src/com/safapp/repositories/IBaseEntityRepository.java
@@ -1,5 +1,17 @@
 package com.safapp.repositories;
 
-public interface IBaseEntityRepository {
+import java.util.List;
+import java.util.UUID;
 
+public interface IBaseEntityRepository<T> {
+
+	public UUID save(T t) throws Exception;
+	
+	public List<T> getAll() throws Exception;
+	
+	public T getById(UUID id) throws Exception;
+	
+	public int delete() throws Exception;
+	
+	public int deleteById() throws Exception;
 }
