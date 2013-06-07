aa428447d19fd847b074d853edd5b4fa26151829
pmunene <rickmune@gmail.com>
2013-06-07 18:06:16 +0300

    httputils to send new url value format, without value string

--- Android/src/com/safapp/utils/http/HttpUtils.java
+++ Android/src/com/safapp/utils/http/HttpUtils.java
@@ -97,12 +97,13 @@ private static final String Tag = "HttpUtils";
 			String afterUlr = "";
 			if(table != null && !table.isEmpty()){
 				Enumeration<String> name = table.keys();
-				int x = 0;
+				//int x = 0;
 				while(name.hasMoreElements()){
 					String key = name.nextElement();
 					String value = table.get(key);
-					afterUlr += (x == 0 ? "?" : "&") + key + "=" + URLEncoder.encode(value);
-					x++;
+					afterUlr += "/" + URLEncoder.encode(value);
+					//afterUlr += (x == 0 ? "?" : "&") + key + "=" + URLEncoder.encode(value);
+					//x++;
 				}
 			}
 			url += afterUlr;
